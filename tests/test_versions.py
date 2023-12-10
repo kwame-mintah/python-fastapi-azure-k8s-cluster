4db4d4378f1ed7976301e98faa64c1f267e925fd
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-12-10 16:28:06 +0000

    test(versions): change python assert for v3.12
    self hosted python version often updates to the latest python minor version of major release, causing unit test to fail.

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -8,10 +8,9 @@ client = TestClient(app)
 def test_get_python_version():
     response = client.get("/v1/version/python")
     assert response.status_code == 200
-    assert response.json() == {
-        "version": "sys.version_info(major=3, minor=12, micro=0, "
-        "releaselevel='final', serial=0)"
-    }
+    is_python_3_12 = str(response.json()).count("major=3, minor=12")
+    # Slightly different assert as azure agent updates minor version(s) during run.
+    assert is_python_3_12 == 1
 
 
 def test_get_fastapi_version():
